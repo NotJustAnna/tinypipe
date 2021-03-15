5f290ef6288e444245888f562e7f5806bce7e2b0
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2021-03-15 08:25:14 -0300

    Initial commit

---
+++ src/main/java/net/notjustanna/tinypipe/Pipe.java
@@ -0,0 +1,11 @@
+package net.notjustanna.tinypipe;
+
+import java.io.Closeable;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface Pipe extends Closeable {
+    InputStream inputStream();
+
+    OutputStream outputStream();
+}
